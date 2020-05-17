f7bedd42e3c8bf656f97cc51a79bc8772fa1b46a
chulhee23 <chulhee2351@gmail.com>
2020-05-18 01:27:31 +0900

    add icon

--- App.js
+++ App.js
@@ -5,46 +5,49 @@ import Loading from "./Loading";
 import * as Location from 'expo-location';
 import axios from 'axios';
 import Weather from './Weather';
+import {Ionicons} from "@expo/vector-icons"
 
 const API_KEY = "16e4dac032cb71175bccaeca3c5b9bbd";
 
 export default class extends React.Component {
   state = {
     isLoading: true
-  }
-  
+  };
   getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
     this.setState({
       isLoading: false,
-      temp: data.main.temp
-    })
-
-  }
-
+      condition: weather[0].main,
+      temp
+    });
+  };
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
-      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
-      
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-
-      this.setState({isLoading: false});
-      
     } catch (error) {
-      
-      Alert.alert("Can't find you", "So sad");
+      Alert.alert("Can't find you.", "So sad");
     }
-  }
-  componentDidMount(){
+  };
+  componentDidMount() {
     this.getLocation();
-    
   }
   render() {
-    const {isLoading, temp} =this.state;
-    console.log(temp)
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)}/>;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+        <Weather temp={Math.round(temp)} condition={condition} />
+      );
   }
 }
--- Weather.js
+++ Weather.js
@@ -1,24 +1,54 @@
-import React from 'react'
-import { StyleSheet, Text, View } from "react-native";
-import propTypes from "prop-types";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons"
 
-export default function Weather({temp}){
+
+export default function Weather({ temp }) {
   return (
     <View style={styles.container}>
-      <Text>{temp}</Text>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons size={100} name="weather-lightning-rainy" />
+
+        <Text style={styles.temp}>{temp}*C</Text>
+      </View>
+      <View style={styles.halfContainer}>
+        
+      </View>
     </View>
-  )
+  );
 }
 
 Weather.propTypes = {
-  temp:propTypes.number.isRequired
-}
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Dust"
+  ]).isRequired
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
-
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  temp: {
+    fontSize: 36,
+  
   }
-})
+
+});
